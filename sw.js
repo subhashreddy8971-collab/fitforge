2504a4be2247e220bc1af31c9321ee1cbd51e4cd
subhash <subhashreddy8971@gmail.com>
2026-06-17 15:36:15 +0530

    De-emoji the whole app: SVG line icons across all screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitforge-v5';
+const CACHE = 'fitforge-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => e.waitUntil(
