6561e5fb965c1ba37af3768be3bbf39e32fcf4d3
subhash <subhashreddy8971@gmail.com>
2026-06-18 18:35:33 +0530

    fix(ble): correct BluetoothLe plugin name and raw API usage for scale sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitforge-v8';
+const CACHE = 'fitforge-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => e.waitUntil(
